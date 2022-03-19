3174ea37fb3505681185c93095f86105250d26f5
brunogjf <bruno.gabriel@facc.ufjf.br>
2022-03-19 16:32:08 -0300

    update

--- Nível1/Exercicio5/script.js
+++ Nível1/Exercicio5/script.js
@@ -1,2 +1,8 @@
 alert('Seja bem vindo(a) ao meu site!');
 
+function clique(){
+    num = prompt('Insira um número:' );
+    dobro = num*2;
+    metadde = num/2; 
+    document.getElementById('resultado').innerHTML= 'O dobro de ' + ' '+  num + ' ' +'é' + ' '+ dobro + ' ' + 'e a metade é' + ' ' + metadde + '!'; 
+}; 
